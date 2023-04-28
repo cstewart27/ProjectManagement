28790d36bfec9a8269d13029a774db4550d9ab28
c_stewart27 <cstewart3@student.sjcny.edu>
2023-04-28 13:10:14 -0400

    initialCommit

---
+++ src/AdminHome.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class AdminHome {
+    private JComboBox selectEmployeeComboBox;
+    private JComboBox selectTaskComboBox;
+    private JTextArea taskDescriptTextArea;
+    private JComboBox selectTaskProgressComboBox;
+    private JTextField defaultDueDateTextField;
+    private JComboBox selectPaymentStatusComboBox;
+    private JButton updateCurrentTaskButton;
+    private JButton assignNewTaskButton;
+    private JTextField textField1;
+}
